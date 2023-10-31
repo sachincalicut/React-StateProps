0f72a731a181af9eb36b6e4fede18faf845b32fb
Sachin Chanduveetil <sachisavera@gmail.com>
2023-10-31 11:14:49 +0530

    React Child to Parent and Parent to Child Pass Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ParentToChild from './ParentToChild/ParentToChild';
+import ChildToParent from './ChildToParent/ChildToParent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Parent To Child */}
+        <ParentToChild/>
+
+        {/* Child to Parent */}
+        <ChildToParent/>
     </div>
-  );
+)
 }
 
 export default App;
